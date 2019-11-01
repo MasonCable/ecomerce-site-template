d4af953682ca5a49312ff8766968a3bb0e8d578a
MasonCable <masoncablelsc@gmail.com>
2019-11-01 16:17:29 -0700

    Made change to tile

--- src/App.js
+++ src/App.js
@@ -21,18 +21,18 @@ class App extends Component {
     return (
         <Provider store={store}>
             <BrowserRouter>
-            <React.Fragment>
-                <Header/>
-                <Switch>
-                    <Route exact path={'/'} render={() => {
-                        return <Redirect to={'/products'}/>
-                    }}/>
-                    <Route exact path={'/products'} component={Home}/>
-                    <Route exact path={'/products/:id'} component={ProductDetail}/>
-                    <Route exact patr={'/cart'} component={ShoppingCart}/>
-                </Switch>
-                <Footer/>
-            </React.Fragment>
+                <React.Fragment>
+                    <Header/>
+                        <Switch>
+                            <Route exact path={'/'} render={() => {
+                                return <Redirect to={'/products'}/>
+                            }}/>
+                            <Route exact path={'/products'} component={Home}/>
+                            <Route exact path={'/products/:id'} component={ProductDetail}/>
+                            <Route exact patr={'/cart'} component={ShoppingCart}/>
+                        </Switch>
+                    <Footer/>
+                </React.Fragment>
             </BrowserRouter>
         </Provider>
     );
